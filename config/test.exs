28c6a4b11001bdb74ba488c33d5558012cfa7425
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2023-01-23 17:16:36 -0500

    remove unused config

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,5 @@
 import Config
 
 config :opentelemetry,
-  #sampler: {:always_on, %{}},
-  #tracer: :otel_tracer_default,
   span_processor: :batch,
   traces_exporter: {:otel_exporter_stdout, []}
-  #processors: [
-  #  otel_batch_processor: %{scheduled_delay_ms: 1, exporter: :undefined}
-  #]
